3eb9337b7dec5fda2d60e6009c12440b87591aca
Deepak <vipin.webperfection@gmail.com>
2025-08-01 18:29:05 +0530

    working on login issue

--- src/app/api/currentuser/route.js
+++ src/app/api/currentuser/route.js
@@ -2,10 +2,15 @@ import { cookies } from 'next/headers';
 
 export async function GET() {
   
-  const cookieStore = cookies(); // ✅ await required
-  const cookieHeader = cookieStore.toString();
+  // const cookieStore = cookies(); // ✅ await required
+  // const cookieHeader = cookieStore.toString();
   // const cookieHeader = cookies().toString();
-console.log('cookieHeader:', cookieHeader); // ✅ Debug this
+   const cookieHeader = request.headers.get('cookie')
+   console.log('cookieHeader:', cookieHeader); // ✅ Debug this
+   
+    if (!cookieHeader) {
+      return new Response(JSON.stringify({ user: null, 'data': 'no data' }), { status: 401 });
+    }
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/currentuser`, {
     method: 'GET',
     headers: {
