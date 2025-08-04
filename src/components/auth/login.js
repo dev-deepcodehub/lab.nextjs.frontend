fee40d3b329708c793e53681d21c43a321aef384
Deepak <vipin.webperfection@gmail.com>
2025-08-04 12:15:27 +0530

    working on login issue -deep - office

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -15,7 +15,7 @@ import { EyeIcon } from '@phosphor-icons/react/dist/ssr/Eye';
 import { EyeSlashIcon } from '@phosphor-icons/react/dist/ssr/EyeSlash';
 import { paths } from '@/paths';
 import { loginUser } from '@/lib/authapi/api'; 
-import { getCurrentUser } from '@/app/api/userapi'
+import { getCurrentUser } from '@/lib/authapi/userwithsession'; 
 // import { useAuth } from '@/context/user-context'
 
 
