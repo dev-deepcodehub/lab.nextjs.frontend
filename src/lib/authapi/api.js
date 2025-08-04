04ca2209c4b3535a6beae93ca2aa05e0f7188fb6
Deepak <vipin.webperfection@gmail.com>
2025-08-04 12:48:24 +0530

    working on login issue -deep - office

--- src/lib/authapi/api.js
+++ src/lib/authapi/api.js
@@ -3,8 +3,7 @@ import axios from 'axios';
 
 // Axios instance
 const api = axios.create({
-  // baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
-  baseURL: 'https://dev.digilabpro.com',
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   withCredentials: true,
 }); 
 
@@ -25,11 +24,11 @@ export const logOut = async () => {
   return response.data;
 };
 
-// // Get current user from session
-// export const getCurrentUser = async () => {
-//   const response = await api.get('/currentuser');
-//   return response.data;
-// };
+// Get current user from session
+export const getCurrentUser = async () => {
+  const response = await api.get('/currentuser');
+  return response.data;
+};
 
 // Export the Axios instance too (optional, if needed elsewhere)
 export default api;
